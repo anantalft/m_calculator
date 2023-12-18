bf2f75c66737f6611eb52890c23d5fa44bb42e97
ananta <anantalamichhane1@gmail.com>
2023-12-18 17:12:59 -0500

    render.yml updated

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module Quoting
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.active_record.migration_error = :page_load
 
     # Configuration for the application, engines, and railties goes here.
     #
